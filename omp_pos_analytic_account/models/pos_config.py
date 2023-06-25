82c4243b060997b275765bb8574cd70024bb9d2e
muram-105 <muram8700@gmail.com>
2023-06-25 13:51:19 +0300

    s

--- omp_pos_analytic_account/models/pos_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class pos_config(models.Model):
-    _inherit = 'pos.config'
-
-    account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account')
